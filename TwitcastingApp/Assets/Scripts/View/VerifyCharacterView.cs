16e923e19a4296d67532dcb81601e9341c3c0b71
Aguri <aguri1120@gmail.com>
2017-01-28 12:55:57 +0900

    能力行使するところまで

---
+++ TwitcastingApp/Assets/Scripts/View/VerifyCharacterView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VerifyCharacterView : MonoBehaviour {
+
+	public Text characterNameText;
+	public Text characterFlavorText;
+	public Image characterImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void SetView(string name, string flavor) {
+		characterNameText.text = name;
+		characterFlavorText.text = flavor;
+	}
+}
---
+++ TwitcastingApp/Assets/Scripts/View/ViewBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewBase : MonoBehaviour {
+
+	public virtual void SetView () {
+	}
+}
